01b6ff9e36359fa6faae4ed57f8970064f1d3a32
Jeet Paul <jeetpaul2222@gmail.com>
2025-11-07 13:41:45 +0530

    Add outline-none class to TaskList container for improved styling

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -4,7 +4,7 @@ const TaskList = () => {
   return (
     <div
       id="tasklist"
-      className="h-[50%] flex items-center  justify-start gap-5 flex-nowrap md:mx-20 mx-10 md:pt-10 overflow-x-auto"
+      className="h-[50%] flex items-center  justify-start gap-5 flex-nowrap md:mx-20 mx-10 md:pt-10 overflow-x-auto outline-none"
     >
       <div className="shrink-0 md:h-full h-80 w-[350px] md:w-[265px] bg-yellow-400 rounded-xl"></div>
       <div className="shrink-0 md:h-full h-80 w-[350px] md:w-[265px] bg-yellow-400 rounded-xl"></div>
